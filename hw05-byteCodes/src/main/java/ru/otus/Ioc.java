b405705bc9c8711d3b386e56ced26e8b99df4e7d
gogolevdv <d.gogolev@gmail.com>
2020-09-02 10:33:14 +0300

    Домашнее задание от 20200804 (refactoring)

--- hw05-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw05-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -3,17 +3,19 @@ package ru.otus;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ioc {
 
     // Набор для имен и типов аргументов методов с аннотацией
-    Set<String> methodIsLogTrue = new HashSet<>();
+    private final Set<String> methodIsLogTrue = new HashSet<>();
 
     Ioc() {
     }
 
-     CalcInterface createMyClass() throws ClassNotFoundException {
+    CalcInterface createMyClass() throws ClassNotFoundException {
 
         Class<?> clazz = Class.forName("ru.otus.CalcImpl");
 
@@ -22,8 +24,7 @@ public class Ioc {
         for (Method md : methods) {
             if (md.isAnnotationPresent(Log.class)) {
                 // методы с аннотацией помещаем в set
-                var nameParamList = md.getName().concat(Arrays.toString(md.getParameterTypes()));
-                methodIsLogTrue.add(nameParamList);
+                methodIsLogTrue.add(formationNamePlusArgsMethod(md));
             }
         }
 
@@ -34,7 +35,7 @@ public class Ioc {
 
     }
 
-     class DemoInvocationHandler implements InvocationHandler {
+    class DemoInvocationHandler implements InvocationHandler {
         private final CalcInterface myClass;
 
         DemoInvocationHandler(CalcInterface myClass) {
@@ -44,10 +45,8 @@ public class Ioc {
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-            var nameParamList = method.getName().concat(Arrays.toString(method.getParameterTypes()));
-
             for (String qq1 : methodIsLogTrue) {
-                if (qq1.equals(nameParamList)) {
+                if (qq1.equals(formationNamePlusArgsMethod(method))) {
                     System.out.print("Executed method:" + method.getName() + "; Params: ");
                     for (Object qq : args) System.out.print(qq + " ");
                     System.out.println();
@@ -57,5 +56,9 @@ public class Ioc {
             return method.invoke(myClass, args);
         }
 
+    }
+
+    public String formationNamePlusArgsMethod(Method method) {
+        return method.getName().concat(Arrays.toString(method.getParameterTypes()));
     }
 }
