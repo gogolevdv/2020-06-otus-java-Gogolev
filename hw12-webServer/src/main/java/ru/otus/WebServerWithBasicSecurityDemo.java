35bc4716d85b3fc87190ae68b8d123e819c99578
gogolevdv <gogolevdv@nspk.ru>
2020-10-27 01:31:29 +0300

    Домашнее задание от 29 сентября 2020 (Работа над ошибками 1)

--- hw12-webServer/src/main/java/ru/otus/WebServerWithBasicSecurityDemo.java
+++ hw12-webServer/src/main/java/ru/otus/WebServerWithBasicSecurityDemo.java
@@ -57,75 +57,15 @@ public class WebServerWithBasicSecurityDemo {
         MigrationsExecutor migrationsExecutor = new MigrationsExecutorFlyway(HIBERNATE_CFG_FILE);
         migrationsExecutor.executeMigrations();
 
+
         SessionFactory sessionFactory = HibernateUtils.buildSessionFactory(HIBERNATE_CFG_FILE, User.class, AddressDataSet.class, PhoneDataSet.class);
 
         SessionManagerHibernate sessionManager = new SessionManagerHibernate(sessionFactory);
         UserDao userDao = new UserDaoHibernate(sessionManager);
         DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
 
+        addUsers(dbServiceUser);
 
-        User user1 = new User(0, "Вася", 35, "user1", "user1");
-        User user2 = new User(0, "Вася_2", 36, "user2", "user2");
-        User user3 = new User(0, "Вася_3", 37, "user3", "user3");
-        User user4 = new User(0, "Вася_4", 38, "user4", "user4");
-        User user5 = new User(0, "Вася_5", 39, "user5", "user5");
-        User user6 = new User(0, "Вася_6", 44, "user5", "user5");
-        User user7 = new User(0, "Вася_7", 55, "user5", "user5");
-
-        AddressDataSet addressDataSet1 = new AddressDataSet();
-        PhoneDataSet phoneDataSet1 = new PhoneDataSet();
-        addressDataSet1.setStreet("123");
-        Set<PhoneDataSet> phones1 = new HashSet<>();
-        phoneDataSet1.setNumber("1234567");
-        phoneDataSet1.setNumber("qwqrwer");
-        phoneDataSet1.setUser(user1);
-        phones1.add(phoneDataSet1);
-
-        user1.setAddress(addressDataSet1);
-        user1.setPhone(phones1);
-
-        AddressDataSet addressDataSet2 = new AddressDataSet();
-        addressDataSet2.setStreet("234");
-        user2.setAddress(addressDataSet2);
-
-        AddressDataSet addressDataSet3 = new AddressDataSet();
-        addressDataSet3.setStreet("123");
-        user3.setAddress(addressDataSet3);
-
-        AddressDataSet addressDataSet4 = new AddressDataSet();
-        addressDataSet4.setStreet("123");
-        user4.setAddress(addressDataSet4);
-
-        AddressDataSet addressDataSet5 = new AddressDataSet();
-        addressDataSet5.setStreet("123");
-        user5.setAddress(addressDataSet5);
-
-        AddressDataSet addressDataSet6 = new AddressDataSet();
-        addressDataSet6.setStreet("123");
-        user6.setAddress(addressDataSet6);
-
-        AddressDataSet addressDataSet7 = new AddressDataSet();
-        addressDataSet7.setStreet("123");
-        user7.setAddress(addressDataSet7);
-
-
-        long id = dbServiceUser.saveUser(user1);
-        Optional<User> mayBeCreatedUser = dbServiceUser.getUser(id);
-        outputUserOptional("Created user", mayBeCreatedUser);
-
-        id = dbServiceUser.saveUser(user2);
-        mayBeCreatedUser = dbServiceUser.getUser(id);
-        outputUserOptional("Created user", mayBeCreatedUser);
-
-        id = dbServiceUser.saveUser(user3);
-
-        mayBeCreatedUser = dbServiceUser.getUser(id);
-        outputUserOptional("Created user", mayBeCreatedUser);
-
-        id = dbServiceUser.saveUser(user4);
-        id = dbServiceUser.saveUser(user5);
-        id = dbServiceUser.saveUser(user6);
-        id = dbServiceUser.saveUser(user7);
 
         Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
         TemplateProcessor templateProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
@@ -141,10 +81,20 @@ public class WebServerWithBasicSecurityDemo {
         usersWebServer.join();
     }
 
-    private static void outputUserOptional(String header, Optional<User> mayBeUser) {
-        System.out.println("-----------------------------------------------------------");
-        System.out.println(header);
-        mayBeUser.ifPresentOrElse(System.out::println, () -> logger.info("User not found"));
+
+
+    private static void addUsers(DBServiceUser userService) {
+
+        User user1 = new User(0, "Вася_1", 35, "user1", "user1");
+        User user2 = new User(0, "Вася_2", 36, "user2", "user4");
+        User user3 = new User(0, "Вася_3", 37, "user3", "user3");
+        User user4 = new User(0, "Вася_4", 38, "user4", "user5");
+
+        userService.saveUser(user1);
+        userService.saveUser(user2);
+        userService.saveUser(user3);
+        userService.saveUser(user4);
+
     }
 
 }
